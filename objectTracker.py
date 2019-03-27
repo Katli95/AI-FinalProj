3cf242d22270cce2bb073d46a6e52eb2c5bbc5ad
Ketill Antoníus Ágústsson <ketill@ozio.is>
2019-03-27 21:45:33 +0000

    small modifications

--- objectTracker.py
+++ objectTracker.py
@@ -1,10 +1,17 @@
 from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers.convolutional import Convolution2D
 from keras.layers import MaxPool2D
 from keras import regularizers
 
 import xml.etree.ElementTree as ET
+
+# Constants
+GridW = GridH = 7
+NumBoundingBoxes = 2
+Classes = ["Sphere", "Can", "Bottle"]
+BoundingBoxOverhead = 5 # 4 for x,y,w,h and 1 for confidence
+
 
 def getNetwork():
     reg = regularizers.l2()
@@ -46,9 +53,10 @@ def getNetwork():
     model.add(Convolution2D(1024, (3,3), 1, padding="same", kernel_regularizer=reg))
 
     model.add(Flatten())
-    model.add(Dense(50176, activation="relu"))
-    model.add(Dense(4096, activation="relu"))
-    model.add(Dense(7*7*13, activation="relu"))
+    model.add(Dense(4096, activation="softmax"))
+    #TODO: Add dropout
+    model.add(Dense(GridW*GridH+(NumBoundingBoxes*BoundingBoxOverhead+ len(Classes)), activation="softmax"))
+    model.add(Reshape([GridW,GridH, (NumBoundingBoxes*BoundingBoxOverhead) + len(Classes)]))
 
     return model
 

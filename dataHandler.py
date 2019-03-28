89901d692426d781435628048221ccf962b3f94b
Ketill Antoníus Ágústsson <ketill@ozio.is>
2019-03-28 10:55:42 +0000

    moved read_content to dataHandler

---
+++ dataHandler.py
@@ -0,0 +1,27 @@
+import xml.etree.ElementTree as ET
+
+def read_content(xml_file: str):
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    list_with_all_boxes = []
+
+    for boxes in root.iter('object'):
+
+        filename = root.find('filename').text
+
+        ymin, xmin, ymax, xmax = None, None, None, None
+
+        for box in boxes.findall("bndbox"):
+            ymin = int(box.find("ymin").text)
+            xmin = int(box.find("xmin").text)
+            ymax = int(box.find("ymax").text)
+            xmax = int(box.find("xmax").text)
+
+        list_with_single_boxes = [xmin, ymin, xmax, ymax]
+        list_with_all_boxes.append(list_with_single_boxes)
+
+    return filename, list_with_all_boxes
+
+name, boxes = read_content("file.xml")
--- objectTracker.py
+++ objectTracker.py
@@ -4,8 +4,6 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers import MaxPool2D
 from keras import regularizers
 
-import xml.etree.ElementTree as ET
-
 # Constants
 GridW = GridH = 7
 NumBoundingBoxes = 2
@@ -59,29 +57,3 @@ def getNetwork():
     model.add(Reshape([GridW,GridH, (NumBoundingBoxes*BoundingBoxOverhead) + len(Classes)]))
 
     return model
-
-def read_content(xml_file: str):
-
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-
-    list_with_all_boxes = []
-
-    for boxes in root.iter('object'):
-
-        filename = root.find('filename').text
-
-        ymin, xmin, ymax, xmax = None, None, None, None
-
-        for box in boxes.findall("bndbox"):
-            ymin = int(box.find("ymin").text)
-            xmin = int(box.find("xmin").text)
-            ymax = int(box.find("ymax").text)
-            xmax = int(box.find("xmax").text)
-
-        list_with_single_boxes = [xmin, ymin, xmax, ymax]
-        list_with_all_boxes.append(list_with_single_boxes)
-
-    return filename, list_with_all_boxes
-
-name, boxes = read_content("file.xml")

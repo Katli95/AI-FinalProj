08250277060bfafafdcc78233287c83cb8c00f7c
Ketill Antoníus Ágústsson <ketill@ozio.is>
2019-04-03 16:57:27 +0000

    flaw in batchGenerator

--- dataHandler.py
+++ dataHandler.py
@@ -113,22 +113,26 @@ class BatchGenerator(Sequence):
                     break
                 if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin']:
                     center_x = (obj['xmin'] + obj['xmax'])/2
-                    center_x = center_x / (float(self.config['IMAGE_W']) / self.config['GRID_W'])
+                    center_x_relative_to_image = center_x / train_instance['width']
+                    center_x_in_box_units = center_x_relative_to_image * self.config['GRID_W']
+                    
                     center_y = (obj['ymin'] + obj['ymax'])/2
-                    center_y = center_y / (float(self.config['IMAGE_H']) / self.config['GRID_H'])
+                    center_y_relative_to_image = center_y / train_instance['height']
+                    center_y_in_box_units = center_y_relative_to_image * self.config['GRID_H']
+                    # center_y = center_y / (float(self.config['IMAGE_H']) / self.config['GRID_H'])
 
-                    grid_x = int(np.floor(center_x))
-                    grid_y = int(np.floor(center_y))
-                    center_x = center_x % 1.
-                    center_y = center_y % 1.
+                    grid_x = int(np.floor(center_x_in_box_units))
+                    grid_y = int(np.floor(center_y_in_box_units))
+                    center_x_rel_to_box = center_x % 1.
+                    center_y_rel_to_box = center_y % 1.
 
                     if grid_x < self.config['GRID_W'] and grid_y < self.config['GRID_H']:
                         obj_indx  = self.config['LABELS'].index(obj['name'])
                         
-                        center_w = (obj['xmax'] - obj['xmin']) / (float(self.config['IMAGE_W']) / self.config['GRID_W']) # unit: grid cell
-                        center_h = (obj['ymax'] - obj['ymin']) / (float(self.config['IMAGE_H']) / self.config['GRID_H']) # unit: grid cell
+                        center_w = ((obj['xmax'] - obj['xmin']) / float(train_instance['width'])) * self.config['GRID_W'] # unit: grid cell
+                        center_h = ((obj['ymax'] - obj['ymin']) / float(train_instance['height'])) * self.config['GRID_H'] # unit: grid cell
                         
-                        box = [center_x, center_y, center_w, center_h]
+                        box = [center_x_rel_to_box, center_y_rel_to_box, center_w, center_h]
                         if y_batch[instance_count, grid_y, grid_x, nextBoxIndex, 4] == 1:
                             nextBoxIndex+=1
 
@@ -142,8 +146,8 @@ class BatchGenerator(Sequence):
                 # plot image and bounding boxes for sanity check
                 for obj in all_objs:
                     if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin']:
-                        cv2.rectangle(img[:,:,::-1], (obj['xmin'],obj['ymin']), (obj['xmax'],obj['ymax']), (255,0,0), 3)
-                        cv2.putText(img[:,:,::-1], obj['name'], 
+                        cv2.rectangle(img, (obj['xmin'],obj['ymin']), (obj['xmax'],obj['ymax']), (255,0,0), 3)
+                        cv2.putText(img, obj['name'], 
                                     (obj['xmin']+2, obj['ymin']+12), 
                                     0, 1.2e-3 * img.shape[0], 
                                     (0,255,0), 2)
